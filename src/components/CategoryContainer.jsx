21960cd72a985c8a58cdf38dfc3c5c425e9e9288
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-11-01 22:00:44 +0000

    Adding images on our container

--- src/components/CategoryContainer.jsx
+++ src/components/CategoryContainer.jsx
@@ -2,7 +2,7 @@ import CategoryItemList from "./CategoryItemList";
 
 function CategoryContainer() {
   const categories = [
-    { id: 1, title: "Hats" },
+    { id: 1, title: "Hats", imageUrl: "https://i.ibb.co/cvpntL1/hats.png" },
     { id: 2, title: "Jackets" },
     { id: 3, title: "Sneakers" },
     { id: 4, title: "Women" },

58461232393c46f3d7f0d409804d011d1649130d
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-11-01 23:14:50 +0000

    Background images on containers

--- src/components/CategoryItemList.jsx
+++ src/components/CategoryItemList.jsx
@@ -2,7 +2,11 @@ function CategoryItemList({ categoryName, id, imageUrl }) {
   console.log(imageUrl);
   return (
     <div className="category-item">
-      <div className={`category-image bg-[url(${imageUrl})]`}></div>
+      <div
+        className="category-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+
       <div className="category-text z-10">
         <h2>{categoryName}</h2>
         <p>Show Now</p>

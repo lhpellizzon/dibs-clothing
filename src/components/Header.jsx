c66ae22b5cd51a0fdc645db323b1f5b96b652d16
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-11-03 14:23:09 +0000

    Working on NavBar Layout with Links and routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,32 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 function Header() {
   return (
     <>
       <nav className="nav-container">
-        <div className="bg-red-500">
-          <h1 className="">Hello </h1>
+        <div className="nav-log">
+          <Link to="/">
+            <img src="" alt="" />
+          </Link>
+        </div>
+
+        <div className="nav-links">
+          <ul>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/login">Sign In</Link>
+            </li>
+            <li>
+              <Link to="/checkout">
+                <img src="" alt="" />
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
       <Outlet />

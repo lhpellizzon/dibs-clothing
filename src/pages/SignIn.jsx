020ecf6e0c4c3bb80a66834af0f189ccd1271db1
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-11-05 20:05:33 +0000

    Sign In UI Complete

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -49,12 +49,12 @@ function SignIn() {
       </Helmet>
       <section id="signin" className="">
         <div className="container mx-auto mt-16 flex items-center justify-center">
-          <div className="w-96 mx-2 rounded-lg  bg-amber-50">
+          <div className="w-96 mx-2 rounded-lg  bg-slate-900">
             <form className="flex flex-col items-center px-8 py-4" onSubmit={handleSubmit}>
-              <h1 className="text-3xl font-bold  mb-3 w-full text-center py-6">Sign In</h1>
+              <h1 className="text-3xl font-bold  mb-3 w-full text-center py-6 text-amber-50">Sign In</h1>
               <div className="mb-6 min-w-full">
                 <button
-                  className="p-3 w-full rounded-md flex items-center justify-center gap-2 bg-slate-300 hover:bg-slate-200 active:scale-95"
+                  className="p-3 w-full rounded-md flex items-center justify-center gap-2 text-slate-900 bg-slate-300 hover:bg-slate-200 active:scale-95"
                   onClick={googleLogin}
                 >
                   <FcGoogle className="text-2xl" />
@@ -62,7 +62,9 @@ function SignIn() {
                 </button>
               </div>
               <div className="flex flex-col gap-2 min-w-full mb-6">
-                <label htmlFor="email">Enter Email</label>
+                <label htmlFor="email" className="text-amber-50">
+                  Enter Email
+                </label>
                 <input
                   className="p-2 w-full rounded-md border-black border-x border-y"
                   type="email"
@@ -74,7 +76,9 @@ function SignIn() {
                 />
               </div>
               <div className="flex flex-col gap-2 min-w-full mb-6">
-                <label htmlFor="password">Enter Password</label>
+                <label htmlFor="password" className="text-amber-50">
+                  Enter Password
+                </label>
                 <input
                   className="p-2 w-full rounded-md border-black border-x border-y"
                   type="password"
@@ -86,11 +90,11 @@ function SignIn() {
                 />
               </div>
               <input
-                className="min-w-full mt-4 mb-2 rounded-md bg-slate-900 font-semibold text-white uppercase py-4 hover:cursor-pointer hover:bg-slate-600 active:scale-95"
+                className="min-w-full mt-4 mb-2 rounded-md bg-amber-50 font-semibold text-slate-900 uppercase py-4 hover:cursor-pointer hover:bg-slate-200 active:scale-95"
                 type="submit"
                 value="Submit"
               />
-              <div className="min-w-full text-center">
+              <div className="min-w-full text-center text-amber-50">
                 <p>Don't have an account?</p>
                 <Link to="/signup" className="underline text-blue-500 hover:text-blue-300">
                   Click Here

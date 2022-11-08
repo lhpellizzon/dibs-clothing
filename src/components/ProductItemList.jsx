1478d4e38e4884b0725e4bbba54264f48ad6b775
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-11-08 14:42:07 +0000

    Product Cards Styled

--- src/components/ProductItemList.jsx
+++ src/components/ProductItemList.jsx
@@ -2,9 +2,16 @@ const ProductItemList = ({ product }) => {
   const { name, price, imageUrl } = product;
   return (
     <div className="productList">
-      <img src={imageUrl} alt="" className="productList-image" />
-      <h1 className="productList-name">{name}</h1>
-      <p className="productList-price">{price}</p>
+      <div className="productList-imageBody">
+        <img src={imageUrl} alt="" className="productList-image" />
+      </div>
+      <div className="productList-details">
+        <h1 className="productList-name">{name}</h1>
+        <p className="productList-price">{price}€</p>
+      </div>
+      <button type="button" className="productList-button">
+        ADD TO CART
+      </button>
     </div>
   );
 };

291a5bad243aa70da51c8c40740048a2aec8f583
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-11-10 17:16:40 +0000

    Mobile navbar being created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { UserContext } from "../context/GlobalState";
 import Crown from "../assets/imgs/crown.svg";
@@ -6,6 +6,7 @@ import { signOutUser } from "../utils/firebase/firebase.utils";
 import Cart from "./Cart";
 function Header() {
   const { currentUser } = useContext(UserContext);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
@@ -15,8 +16,42 @@ function Header() {
             <img src={Crown} alt="" />
           </Link>
         </div>
+        {/* Desktop menu */}
+        <div className="hidden sm:block">
+          <ul className="nav-links">
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              {currentUser ? (
+                <Link to="/login" onClick={signOutUser}>
+                  Sign Out
+                </Link>
+              ) : (
+                <Link to="/login">Sign In</Link>
+              )}
+            </li>
+            <li>
+              <Cart />
+            </li>
+          </ul>
+        </div>
+        {/* Button */}
+        <button
+          id="menu-btn"
+          className={`${isOpen ? "open" : ""} block hamburger focus:outline-none`}
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          <span className="hamburger-top"></span>
+          <span className="hamburger-mid"></span>
+          <span className="hamburger-bot"></span>
+        </button>
 
-        <div>
+        {/* Mobile menu */}
+        <div className="hidden">
           <ul className="nav-links">
             <li>
               <Link to="/shop">Shop</Link>

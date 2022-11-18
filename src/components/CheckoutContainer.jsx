b64b230bf065555e5daed0ffafba92aa279c4cf8
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-11-18 17:54:26 +0000

    Design Checkout Done. Next Step: Function to add more item or delete item

--- src/components/CheckoutContainer.jsx
+++ src/components/CheckoutContainer.jsx
@@ -5,21 +5,26 @@ import CheckoutItem from "./CheckoutItem";
 const CheckoutContainer = () => {
   const { cartItems } = useContext(CartContext);
   const total = cartItems.reduce((acc, item) => acc + item.quantity * item.price, 0);
-  console.log(total);
+
   return (
     <section id="checkout">
       <div className="container mx-auto flex flex-col items-center text-center py-4">
         <h1 className="text-3xl font-bold uppercase p-4">Checkout</h1>
-        <div className="flex flex-col space-y-5 w-full md:max-w-2xl">
+        <div className="flex flex-col space-y-5 px-4 w-full md:max-w-2xl py-6">
           <ul className="flex flex-col gap-3">
             {cartItems.map((item) => (
               <CheckoutItem key={item.id} item={item} />
             ))}
           </ul>
-          <div className="">
-            <p>Total</p>
-            <span>{total}</span>
+          <hr />
+          <div className="flex justify-between pr-6 uppercase font-bold">
+            <p className="text-2xl pl-10 ">Total</p>
+            <span className="text-2xl">{total}€</span>
           </div>
+
+          <button className="py-3 bg-slate-900 text-amber-50 rounded mx-auto w-[75%] active:scale-[99%]">
+            Go to Payment
+          </button>
         </div>
       </div>
     </section>
--- src/components/CheckoutItem.jsx
+++ src/components/CheckoutItem.jsx
@@ -2,23 +2,23 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const CheckoutItem = ({ item }) => {
   return (
-    <li className="flex gap-4 bg-slate-100 mx-5 rounded pr-6">
+    <li className="flex gap-4 bg-slate-900 rounded pr-6 text-amber-50 font-bold">
       <div>
         <img src={item.imageUrl} alt={item.name} className="w-32 h-40 rounded" />
       </div>
       <div className="flex flex-col justify-evenly flex-auto">
         <div className="flex justify-between">
-          <h2 className="">{item.name}</h2>
-          <span>€{item.price}</span>
+          <h2 className="text-2xl">{item.name}</h2>
+          <span className="text-xl">{item.price}€</span>
         </div>
         <div className="flex justify-between">
-          <p>Quantity</p>
+          <p className="text-sm">Quantity</p>
           <div className="flex items-center">
-            <button>
+            <button className="active:scale-75">
               <FaChevronLeft />
             </button>
-            <p> {item.quantity}</p>
-            <button>
+            <p className="text-xl"> {item.quantity}</p>
+            <button className="active:scale-75">
               <FaChevronRight />
             </button>
           </div>

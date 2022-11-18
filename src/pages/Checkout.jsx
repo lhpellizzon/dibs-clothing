0340fabc9eedffc67d032cfd8f698218c6c7ef50
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-11-18 16:28:30 +0000

    Start working on checkout page, using Cart Context

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import { useContext } from "react";
 import { Helmet } from "react-helmet";
+import { CartContext } from "../context/CartContext";
 
 function Checkout() {
+  const cart = useContext(CartContext);
+  console.log(cart);
   return (
     <>
       <Helmet>

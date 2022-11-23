754ed4360d98fb009864786c1b8af6c508472971
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-11-23 19:00:24 +0000

    Change from json file to js file (SHOP_DATA) containing more products with different categories

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,12 +1,12 @@
 import { createContext, useState } from "react";
-import PRODUCTS from "../shop-data.json";
+import SHOP_DATA from "../shop-data.js";
 
 export const ProductContext = createContext({
   products: [],
 });
 
 export const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState(SHOP_DATA);
   const value = { products };
 
   return <ProductContext.Provider value={value}>{children}</ProductContext.Provider>;
